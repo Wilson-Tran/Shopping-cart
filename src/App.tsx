d8acdaa071aedf4369454c86b238515952e35966
WT <wilsonwttran@gmail.com>
2023-01-15 20:49:56 -0800

    initiial commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Routes, Route} from "react-router-dom"
+import { Container } from "react-bootstrap"
+import { Home } from "./pages/home"
+import { Store } from "./pages/store"
+import { About } from "./pages/about"
+import { Navbar } from "./components/Navbar"
+import { ShoppingCartProvider } from "./context/ShoppingCartContext"
+
+function App() {
+  return (
+    //fragment
+    <ShoppingCartProvider> 
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      
+      </Container>
+    </ShoppingCartProvider>
+  )
+}
+
+export default App
